5d0d41858ae4cdd097f982401c8b96fbbca628ca
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-06-29 16:09:11 -0400

    add logging

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,14 +1,18 @@
 from celery import Celery
 import os
+import logging
+import etd
 
 app = Celery()
 app.config_from_object('celeryconfig')
+etd.configure_logger()
+logger = logging.getLogger('etd_alma')
 
 
 @app.task(serializer='json', name='etd-alma-service.tasks.send_to_alma')
 def send_to_alma(message):
-    print("message")
-    print(message)
+    logger.info("message")
+    logger.info(message)
     new_message = {"hello": "from etd-alma-service"}
     # publish to ingested_into_alma for helloworld,
     # eventually webhooks will do that instead
