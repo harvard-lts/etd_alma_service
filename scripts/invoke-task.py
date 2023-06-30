ae6f63bc5e04a557f31c43e36622913c6f1c099c
vac765 <valdeva_crema@harvard.edu>
2023-06-29 20:14:45 -0400

    feature flag implementation

--- scripts/invoke-task.py
+++ scripts/invoke-task.py
@@ -1,9 +1,17 @@
 from celery import Celery
 import os
+import json
 
 app1 = Celery('tasks')
 app1.config_from_object('celeryconfig')
 
+arguments = {"hello": "world", "feature_flags": {
+            'dash_feature_flag': "off",
+            'alma_feature_flag': "off",
+            'send_to_drs_feature_flag': "off",
+            'drs_holding_record_feature_flag': "off"}}
+json_args = json.dumps(arguments)
+
 res = app1.send_task('etd-alma-service.tasks.send_to_alma',
-                     args=[{"hello": "world"}], kwargs={},
+                     args=[json_args], kwargs={},
                      queue=os.getenv("CONSUME_QUEUE_NAME"))
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -2,6 +2,7 @@ from celery import Celery
 import os
 import logging
 import etd
+import json
 
 app = Celery()
 app.config_from_object('celeryconfig')
@@ -14,6 +15,23 @@ def send_to_alma(message):
     logger.info("message")
     logger.info(message)
     new_message = {"hello": "from etd-alma-service"}
+    if "feature_flags" in json_message:
+        feature_flags = json_message["feature_flags"]
+        new_message["feature_flags"] = feature_flags
+        if "alma_feature_flag" in feature_flags and \
+                feature_flags["alma_feature_flag"] == "on":
+            if "dash_feature_flag" in feature_flags and \
+                    feature_flags["dash_feature_flag"] == "on":
+                # Create Alma Record
+                print("FEATURE IS ON>>>>>CREATE ALMA RECORD")
+            else:
+                print("dash_feature_flag MUST BE ON FOR THE ALMA \
+                    HOLDING TO BE CREATED. dash_feature_flag IS SET TO OFF")
+        else:
+            # Feature is off so do hello world
+            print("FEATURE FLAGS FOUND")
+            print(json_message['feature_flags'])
+
     # publish to ingested_into_alma for helloworld,
     # eventually webhooks will do that instead
     app.send_task("etd-alma-monitor-service.tasks.send_to_drs",
