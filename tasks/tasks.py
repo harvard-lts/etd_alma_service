095137dff0e69575b3e965e719a363cc7e3017f5
vac765 <valdeva_crema@harvard.edu>
2023-06-29 20:14:45 -0400

    feature flag implementation

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,5 +1,6 @@
 from celery import Celery
 import os
+import json
 
 app = Celery()
 app.config_from_object('celeryconfig')
@@ -9,7 +10,25 @@ app.config_from_object('celeryconfig')
 def send_to_alma(message):
     print("message")
     print(message)
+    json_message = json.loads(message)
     new_message = {"hello": "from etd-alma-service"}
+    if "feature_flags" in json_message:
+        feature_flags = json_message["feature_flags"]
+        new_message["feature_flags"] = feature_flags
+        if "alma_feature_flag" in feature_flags and \
+                feature_flags["alma_feature_flag"] == "on":
+            if "dash_feature_flag" in feature_flags and \
+                    feature_flags["dash_feature_flag"] == "on":
+                # Create Alma Record
+                print("FEATURE IS ON>>>>>CREATE ALMA RECORD")
+            else:
+                print("dash_feature_flag MUST BE ON FOR THE ALMA \
+                    HOLDING TO BE CREATED. dash_feature_flag IS SET TO OFF")
+        else:
+            # Feature is off so do hello world
+            print("FEATURE FLAGS FOUND")
+            print(json_message['feature_flags'])
+
     # publish to ingested_into_alma for helloworld,
     # eventually webhooks will do that instead
     app.send_task("etd-alma-monitor-service.tasks.send_to_drs",
