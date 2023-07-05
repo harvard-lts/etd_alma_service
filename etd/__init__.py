9f35d04b677ae1d7a6a9f4a67e027099206e4bf9
michael-lts <michael_vandermillen@harvard.edu>
2023-07-05 11:19:16 -0400

    Etd 185 (#8)
    * add logging
    * add containerid, date, to logname

---
+++ etd/__init__.py
@@ -0,0 +1,35 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+import socket
+from datetime import datetime
+
+LOG_FILE_BACKUP_COUNT = os.getenv('LOG_FILE_BACKUP_COUNT')
+LOG_ROTATION = "midnight"
+
+container_id = socket.gethostname()
+timestamp = datetime.today().strftime('%Y-%m-%d')
+
+
+def configure_logger():  # pragma: no cover
+    log_level = os.getenv("APP_LOG_LEVEL", "WARNING")
+    log_file_path = os.getenv("LOGFILE_PATH",
+                              "/home/etdadm/logs/etd_alma")
+    formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger = logging.getLogger('etd_alma')
+    logger.addHandler(console_handler)
+    if os.getenv("CONSOLE_LOGGING_ONLY", 'false') == 'false':
+        file_handler = TimedRotatingFileHandler(
+            filename=f"{log_file_path}/{container_id}_console_{timestamp}.log",
+            when=LOG_ROTATION,
+            backupCount=LOG_FILE_BACKUP_COUNT
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    logger.setLevel(log_level)
