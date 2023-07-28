d80ff34b1d122e5401822bbc6b61dbc1efb16f1c
chip goines <chip_goines@harvard.edu>
2023-07-28 09:39:25 -0400

    Added healthchecks for alma (#15)
    * Added healthchecks for alma
    
    * suppress unverified https urlib3 warning

--- scripts/healthcheck.py
+++ scripts/healthcheck.py
@@ -2,12 +2,29 @@ import os
 import sys
 import time
 from pathlib import Path
+import requests
+from requests.packages import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # heartbeat script for docker/k8s
 
 hbeat_path = os.getenv("HEARTBEAT_FILE", "/tmp/worker_heartbeat")
 HEARTBEAT_FILE = Path(hbeat_path)
 HEARTBEAT_WINDOW = int(os.getenv("HEARTBEAT_WINDOW", 60))
+ALMA_API_KEY = os.getenv("ALMA_API_KEY")
+ALMA_HEALTHCHECK_URL = os.getenv("ALMA_HEALTHCHECK_URL") + ALMA_API_KEY
 
+# check alma
+try:
+    # need verify false b/c using selfsigned certs
+    r = requests.get(ALMA_HEALTHCHECK_URL, verify=False)
+    if (r.status_code != 200):
+        print("alma healthcheck failed")
+        sys.exit(1)
+except Exception:
+    print("alma healthcheck failed")
+    sys.exit(1)
+
+# check timestamp file
 try:
     current_ts = int(time.time())
     fstats = os.stat(HEARTBEAT_FILE)
