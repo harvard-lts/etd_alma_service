f31847fda56f051070f3d01c7700f0f5210406d9
chip goines <chip_goines@harvard.edu>
2023-08-07 08:58:01 -0400

    Etd 206: trace on proquest id (#18)
    * etd-206
    
    * tests

--- tests/unit/test_tasks.py
+++ tests/unit/test_tasks.py
@@ -10,7 +10,10 @@ class TestTasksClass():
                 'dash_feature_flag': "off",
                 'alma_feature_flag': "off",
                 'send_to_drs_feature_flag': "off",
-                'drs_holding_record_feature_flag': "off"}}
+                'drs_holding_record_feature_flag': "off"},
+                "identifier": "30522803"}
         retval = tasks.send_to_alma(message)
         assert "hello" in retval
         assert "feature_flags" in retval
+        assert "identifier" in retval
+        assert retval["identifier"] == "30522803"
