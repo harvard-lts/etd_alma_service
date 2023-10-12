7c5bffd3c7de4b6e12f0f671ff0d2862f752dbc8
Chip Goines <chip_goines@harvard.edu>
2023-10-12 16:06:27 -0400

    initial commit

---
+++ etd/etds2alma_tables.py
@@ -0,0 +1,75 @@
+# Look-up tables used by our etds2alma_pq.py script to map values found 
+# in the batch's mets file to what is used in the marcxml sent to Alma
+
+# A Python dictionary (or associtive array or hash)
+# to map degree name to what's used in datafield 502 subfield a
+degreeCodeName = {
+	'D.B.A.': 'Doctor of Business Administration',
+	'DBA': 'Doctor of Business Administration',
+	'Th.D.': 'Doctor of Theology',
+	'M Arch': 'Master in Architecture',
+	'MDE': 'Master in Design Engineering',
+	'MDes': 'Master in Design Studies',
+	'MLA': 'Master in Landscape Architecture',
+	'MUP': 'Master in Urban Planning',
+	'MAUD': 'Master of Architecture in Urban Design',
+	'MLAUD': 'Master of Landscape Architecture in Urban Design',
+	'DDes': 'Doctor of Design',
+	'MMSCI': 'Master of Medical Sciences in Clinical Investigation',
+	'MMSc-GHD': 'Master of Medical Sciences in Global Health Delivery',
+	'MMSc IMM': 'Master of Medical Sciences in Immunology',
+	'MMSc-Med Ed': 'Master of Medical Sciences in Medical Education',
+	'M.D.': 'Doctor of Medicine',
+	'MD': 'Doctor of Medicine',
+	'D.P.H.': 'Doctor of Public Health',
+	'DPH': 'Doctor of Public Health',
+	'D.S.': 'Doctor of Science',
+	'DS': 'Doctor of Science',
+	'A.L.M.': 'Master of Liberal Arts',
+	'ALM': 'Master of Liberal Arts',
+	'A.B.': 'Bachelor of Arts',
+	'AB': 'Bachelor of Arts',
+	'S.B.': 'Bachelor of Science',
+	'SB': 'Bachelor of Science',
+	'Ph.D.': 'Doctor of Philosophy',
+	'D.Ed.': 'Doctor of Education',
+	'Ed.L.D.': 'Doctor of Education Leadership',
+	'D.M.Sc.': 'Doctor of Medical Sciences'
+ }
+ 
+# A Python dictionary (or associtive array or hash)
+# to map degree level to degree level tracing used in datafield 830 subfield p
+degreeLevelTracing = {
+	"Bachelor's": "Theses",
+	"Undergraduate": "Theses",
+	"Masters": "Theses",
+	"Doctoral": "Dissertations"
+}
+
+# A Python dictionary (or associtive array or hash) of dictionaries
+# to translate the school name to the term used in the subfield a and b 
+# of the 710 datafield. Also used to set if degree level tracing is used.
+schools = {
+	'Harvard Business School':
+		{'subfield_a': 'Harvard Business School', 'subfield_b': False, 'degree_level_tracing': False},
+	'Harvard College':
+		{'subfield_a': 'Harvard College (1780- )', 'subfield_b': False, 'degree_level_tracing': True},
+	'Harvard Divinity School':
+		{'subfield_a': 'Harvard Divinity School', 'subfield_b': False, 'degree_level_tracing': True},
+	'Harvard Graduate School of Design': 
+		{'subfield_a': 'Harvard University' , 'subfield_b': 'Graduate School of Design', 'degree_level_tracing': False},
+	'Harvard Medical School':
+		{'subfield_a': 'Harvard Medical School' , 'subfield_b': False, 'degree_level_tracing': False},
+	'Harvard T.H. Chan School of Public Health':
+		{'subfield_a': 'Harvard T.H. Chan School of Public Health' , 'subfield_b': False, 'degree_level_tracing': False},
+	'Harvard University Division of Continuing Education':
+		{'subfield_a': 'Harvard University' , 'subfield_b': 'Continuing Education Division', 'degree_level_tracing': True},
+	'Harvard University Engineering and Applied Sciences':
+		{'subfield_a': 'Harvard College (1780- )' , 'subfield_b': False, 'degree_level_tracing': True},
+	'Harvard University Graduate School of Arts and Sciences':
+		{'subfield_a': 'Harvard University' , 'subfield_b': 'Graduate School of Arts and Sciences', 'degree_level_tracing': True},
+	'Harvard University Graduate School of Education':
+		{'subfield_a': 'Harvard University' , 'subfield_b': 'Graduate School of Education', 'degree_level_tracing': True},
+	'Harvard University School of Dental Medicine':
+		{'subfield_a': 'Harvard School of Dental Medicine' , 'subfield_b': False, 'degree_level_tracing': False}
+}
---
+++ etd/schools.py
@@ -0,0 +1,104 @@
+#!/bin/env python3
+#
+# To hold DRS related info for our schools
+#
+# Last updated: 02/09/21
+
+school_info = {
+	'college': {
+			'owner_code': 'HUL.ARCH',
+			'billing_code': 'HUL.ARCH.ETHESIS.SEAS_0001',
+			'urn_authority_path': 'HUL.ARCH',
+			'description': 'Harvard College',
+			'university': 'Harvard College',
+			'etd': False
+			},
+	'dce': {
+			'owner_code': 'HUL.ARCH',
+			'billing_code': 'HUL.ARCH.ETHESIS.DCE_0001',
+			'urn_authority_path': 'HUL.ARCH',
+			'description': 'Extension School',
+			'university': 'Harvard University',
+			'etd': False
+			},
+	'edld': {
+			'owner_code': 'HUL.ARCH',
+			'billing_code': 'HUL.ARCH.EDISS.EDLC_0001',
+			'urn_authority_path': 'HUL.ARCH',
+			'description': 'Graduate School of Education',
+			'university': 'Harvard University',
+			'etd': 'Capstone paper'
+			},
+	'gsas': {
+			'owner_code': 'HUL.ARCH',
+			'billing_code': 'HUL.ARCH.EDISS.PHD_0001',
+			'urn_authority_path': 'HUL.ARCH',
+			'description': 'Graduate School of Arts and Sciences',
+			'university': 'Harvard University',
+			'etd': False
+			},
+	'gsd': {
+			'owner_code': 'GSD.LIBR',
+			'billing_code': 'GSD.LIBR.Theses_0001',
+			'urn_authority_path': 'GSD.LOEB',
+			'description': 'Graduate School of Design',
+			'university': 'Harvard University',
+			'etd': 'Thesis'
+			},
+	'gse': {
+			'owner_code': 'HUL.ARCH',
+			'billing_code': 'HUL.ARCH.EDISS.EDD_0001',
+			'urn_authority_path': 'HUL.ARCH',
+			'description': 'Graduate School of Education',
+			'university': 'Harvard University',
+			'etd': 'Thesis'
+			},
+	'hbs': {
+			'owner_code': 'HBS.BAKR',
+			'billing_code': 'HBS.BAKR.HDTC_0001',
+			'urn_authority_path': 'HBS.BAKER',
+			'description': 'Harvard Business School',
+			'university': 'Harvard University',
+			'etd': 'Dissertation'
+			},
+	'hds': {
+			'owner_code': 'HUL.ARCH',
+			'billing_code': 'HUL.ARCH.EDISS.THD_0001',
+			'urn_authority_path': 'HUL.ARCH',
+			'description': 'Harvard Divinity School',
+			'university': 'Harvard University',
+			'etd': 'Thesis'
+			},
+	'hms': {
+			'owner_code': 'HMS.COUNT',
+			'billing_code': 'HMS.COUNT.HMST_0001',
+			'urn_authority_path': 'HMS.Count',
+			'description': 'Harvard Medical School',
+			'university': 'Harvard University',
+			'etd': 'Thesis'
+			},
+	'hsdm': {
+			'owner_code': 'HMS.COUNT',
+			'billing_code': 'HMS.COUNT.HSDMT_0001',
+			'urn_authority_path': 'HMS.Count',
+			'description': 'Harvard School of Dental Medicine',
+			'university': 'Harvard University',
+			'etd': 'Thesis'
+			},
+	'hsph': {
+			'owner_code': 'HMS.COUNT',
+			'billing_code': 'HMS.COUNT.HSPHT_0001',
+			'urn_authority_path': 'HMS.Count',
+			'description': 'Harvard T.H. Chan School of Public Health',
+			'university': 'Harvard University',
+			'etd': False
+			},
+	'osc': {
+			'owner_code': False,
+			'billing_code': False,
+			'urn_authority_path': False,
+			'description': 'Harvard University Office for Scholarly Communications (DO NOT TURN LIVE)',
+			'university': False,
+			'etd': ''
+			}
+}
