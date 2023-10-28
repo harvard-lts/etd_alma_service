4719f8c2e11bfafd2a4abc4da12c47ca1d3c6ba2
Chip Goines <chip_goines@harvard.edu>
2023-10-28 10:52:04 -0400

    write JM logfile if it does not exist

--- tests/unit/test_tasks.py
+++ tests/unit/test_tasks.py
@@ -7,8 +7,8 @@ class TestTasksClass():
 
     def test_send_to_alma(self):
         message = {"unit_test": "true", FEATURE_FLAGS: {
-                'dash_feature_flag': "off",
-                'alma_feature_flag': "off",
+                'dash_feature_flag': "on",
+                'alma_feature_flag': "on",
                 'send_to_drs_feature_flag': "off",
                 'drs_holding_record_feature_flag': "off"},
                 "identifier": "30522803"}
