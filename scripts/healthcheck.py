b7aae75b0718d130d14e3a77b4b6be7e90ca9715
Dee Dee Crema <valdeva_crema@harvard.edu>
2024-02-20 11:50:20 -0500

    alma healthcheck removal

--- scripts/healthcheck.py
+++ scripts/healthcheck.py
@@ -10,21 +10,9 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 hbeat_path = os.getenv("HEARTBEAT_FILE", "/tmp/worker_heartbeat")
 HEARTBEAT_FILE = Path(hbeat_path)
 HEARTBEAT_WINDOW = int(os.getenv("HEARTBEAT_WINDOW", 60))
-ALMA_API_KEY = os.getenv("ALMA_API_KEY")
-ALMA_HEALTHCHECK_URL = os.getenv("ALMA_HEALTHCHECK_URL") + ALMA_API_KEY
 JOBMON_URL = os.getenv("jobMonitor")
 INSTANCE = os.getenv("INSTANCE", "dev")
 
-# check alma
-try:
-    # need verify false b/c using selfsigned certs
-    r = requests.get(ALMA_HEALTHCHECK_URL, verify=False)
-    if (r.status_code != 200):
-        print("alma healthcheck failed")
-        sys.exit(1)
-except Exception:
-    print("alma healthcheck failed")
-    sys.exit(1)
 
 # check jobmon if prod
 if INSTANCE == "prod":
