a3216e9f4d7acc14a5a5e542d4db81e9145bd10c
Dee Dee Crema <valdeva_crema@harvard.edu>
2024-02-29 10:25:59 -0500

    ff update

--- scripts/invoke-task.py
+++ scripts/invoke-task.py
@@ -5,10 +5,10 @@ app1 = Celery('tasks')
 app1.config_from_object('celeryconfig')
 
 arguments = {"hello": "world", "feature_flags": {
-            'dash_feature_flag': "on",
-            'alma_feature_flag': "on",
-            'send_to_drs_feature_flag': "off",
-            'drs_holding_record_feature_flag': "off"}}
+            'dash_feature_flag': os.getenv("DASH_FEATURE_FLAG"),
+            'alma_feature_flag': os.getenv("ALMA_FEATURE_FLAG"),
+            'send_to_drs_feature_flag':
+            os.getenv("SEND_TO_DRS_FEATURE_FLAG")}}
 
 res = app1.send_task('etd-alma-service.tasks.send_to_alma',
                      args=[arguments], kwargs={},
